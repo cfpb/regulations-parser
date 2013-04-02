54b11b191252e92f151a6b68794061a99de0a794
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-04-02 17:50:56 +0000

    Defined a generic layer generator, and the external citations layer

---
+++ external_citations.py
@@ -0,0 +1,57 @@
+#vim: set encoding=utf-8
+import string
+import urllib
+from collections import defaultdict
+from pyparsing import *
+
+class ExternalCitationParser():
+    #The different types of citations
+    CODE_OF_FEDERAL_REGULATIONS = 'CFR'
+    UNITED_STATES_CODE = 'USC'
+
+    def get_description(self):
+        return ExternalCitationParser.DESCRIPTION
+
+    def get_external_link_parser(self):
+        """ Construct a grammar that parses references/citations to the 
+        United States Code and the Code of Federal Regulations. """
+        uscode_exp = Word(string.digits) + "U.S.C." + Word(string.digits)
+
+        cfr_exp_v1 = Word(string.digits) + "CFR" + "part" + Word(string.digits)
+        cfr_exp_v2 = Word(string.digits) + "CFR" + Word(string.digits) + "." + Word(string.digits)
+        cfr_exp = cfr_exp_v1.setResultsName('V1') ^ cfr_exp_v2.setResultsName('V2')
+
+        parse_all =  uscode_exp.setResultsName('USC') | cfr_exp
+        return parse_all
+
+    def citation_type(self, citation):
+        """ Based on the citation parsed, return the type of the citation. """
+        if citation[1] == 'CFR':
+            return ExternalCitationParser.CODE_OF_FEDERAL_REGULATIONS
+        elif citation[1] == 'U.S.C.':
+            return ExternalCitationParser.UNITED_STATES_CODE
+
+    def reformat_citation(self, citation):
+        """ Strip out unnecessary elements from the citation reference, so that 
+        the various types of citations are presented consistently. """
+        return [c for c in citation if c not in ['U.S.C.', 'CFR', 'part', '.']]
+
+    def parse(self, text):
+        """ Parse the provided text, pulling out all the citations. """
+        parser  = self.get_external_link_parser()
+
+        cm = defaultdict(list)
+        citation_strings = {}
+        for citation, start, end in parser.scanString(text):
+            index = "-".join(citation)
+            cm[index].append([start, end])
+            citation_strings[index] = citation.asList()
+
+        def build_layer_element(k, offsets):
+            layer_element = {'offsets': offsets, 
+                'citation': self.reformat_citation(citation_strings[k]),
+                'citation_type': self.citation_type(citation_strings[k])
+            }
+            return layer_element
+
+        return  [build_layer_element(k, offsets) for k,offsets in cm.items()]
---
+++ layer_generator.py
@@ -0,0 +1,31 @@
+
+class LayerGenerator():
+    def __init__(self, regulation_tree, generator):
+        self.tree = regulation_tree
+        self.layer_generator = generator
+        self.layer = {}
+
+    def generate_layer(self):
+        """ Walk the regulation tree, generating a layer at each node. """
+        self.process_node(self.tree)
+        return self.layer 
+
+    def process_node(self, node):
+        citations_list = self.layer_generator.parse(node['text'])
+        if citations_list:
+            part_index = '-'.join(node['label']['parts'])
+            self.layer[part_index] = citations_list
+
+        for c in node['children']:
+            self.process_node(c)
+
+if __name__ == "__main__":
+    import api_stub
+    import external_citations
+    import json
+
+    reg_json = api_stub.get_regulation_as_json('regulations/rege.json')
+    ext_citation_parser = external_citations.ExternalCitationParser()
+    layer_generator = LayerGenerator(reg_json, ext_citation_parser)
+    layer = layer_generator.generate_layer()
+    print json.dumps(layer)
