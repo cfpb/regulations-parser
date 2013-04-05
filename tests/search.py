9014852677f2ad4015d93fec0d169e090d8d7ac2
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-04-05 21:50:47 +0000

    Added some unnecessary testing to make testing framework work

---
+++ tests/search.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+import internal_citations
+
+class SearchTest(TestCase):
+
+    def test_internal_paragraphs(self):
+        text = u"requirements of paragraphs (c)(3), (d)(2), (e)(1), (e)(3), and (f) of this section"
+        self.assertEquals(True, False)
+
