c1a1c58d22aa7aec638a91e00581cbc407024714
CM Lubinski <cm.lubinski@gmail.com>
2013-05-06 16:43:55 +0200

    JSON tree generation takes path to plain text + part as params

--- generate_tree.py
+++ generate_tree.py
@@ -4,16 +4,22 @@ from parser.tree.appendix.tree import trees_from as appendix_trees
 from parser.tree.interpretation.tree import build as build_interp_tree
 from parser.tree.reg_text import build_reg_text_tree
 from parser.tree.supplement import find_supplement_start
+import sys
 
 if __name__ == "__main__":
-    with codecs.open('rege.txt', encoding='utf-8') as f:
+    if len(sys.argv) < 3:
+        print "Usage: python generate_tree.py path/to/reg.txt part"
+        print " e.g.: python generate_tree.py rege.txt 1005"
+        exit()
+    with codecs.open(sys.argv[1], encoding='utf-8') as f:
         reg = unicode(f.read())
 
     interp = reg[find_supplement_start(reg):]
 
-    reg_tree = build_reg_text_tree(reg, 1005)
-    interp_tree = build_interp_tree(interp, 1005)
-    appendix_trees = appendix_trees(reg, 1005, reg_tree['label'])
+    part = int(sys.argv[2])
+    reg_tree = build_reg_text_tree(reg, part)
+    interp_tree = build_interp_tree(interp, part)
+    appendix_trees = appendix_trees(reg, part, reg_tree['label'])
 
     reg_tree['children'].extend(appendix_trees)
     reg_tree['children'].append(interp_tree)
