80e04ac7083c99d3a93eea3b724d7e9525fe3ac1
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-05-24 20:30:10 +0000

    Fixed #42. Layer no longer grabs emphasized words not at the begining of a paragraph as key terms

--- parser/layer/key_terms.py
+++ parser/layer/key_terms.py
@@ -6,18 +6,32 @@ class KeyTerms(Layer):
         Layer.__init__(self, tree)
         self.pattern = re.compile(ur'.*?<E T="03">([^<]*?)</E>.*?', re.UNICODE) 
 
+    def process_node_text(self, node):
+        """ Take a paragraph, remove the marker, and extraneous whitespaces. """
+        marker = node['label']['parts'][-1]
+        marker = '(%s)' % marker
+        text = node['text']
+
+        text = text.replace(marker, '', 1).strip()
+        return text
+
+    def keyterm_is_first(self, node, keyterm):
+        """ The keyterm should be the first phrase in the paragraph. """
+        node_text = self.process_node_text(node)
+        start = node_text.find(keyterm)
+        tag_length = len("<E T='03'>")
+
+        return start == tag_length
+
     def process(self, node):
         matches = self.pattern.match(node['text'])
         if matches:
             keyterm = matches.groups()[0]
-            start = node['text'].find(keyterm)
-            end = start + len(keyterm)
 
-            offsets = [[start, end]]
-
-            layer_el = [{
-                "key_term": keyterm, 
-                #The first instance of the key term is right one. 
-                "locations": [0]
-            }]
-            return layer_el
+            if self.keyterm_is_first(node, keyterm):
+                layer_el = [{
+                    "key_term": keyterm, 
+                    #The first instance of the key term is right one. 
+                    "locations": [0]
+                }]
+                return layer_el
