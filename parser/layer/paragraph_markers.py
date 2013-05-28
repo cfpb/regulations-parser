6d691db73e6498cdc45c1be3807503f9aebed794
CM Lubinski <cm.lubinski@gmail.com>
2013-05-28 18:29:14 +0200

    Combine paragraph marker parsers

--- parser/layer/paragraph_markers.py
+++ parser/layer/paragraph_markers.py
@@ -4,15 +4,20 @@ class ParagraphMarkers(Layer):
 
     def process(self, node):
         """Look for any leading paragraph markers."""
-        marker = node['label']['parts'][-1]
-
-        if 'Interpretations' in node['label']['parts']:
-            marker = marker + '.'
-        else:
-            marker = '(%s)' % marker
-
+        marker = ParagraphMarkers.marker(node)
         if node['text'].strip().startswith(marker):
             return [{
                 "text": marker,
                 "locations": [0]
             }]
+
+    @staticmethod
+    def marker(node):
+        m = node['label']['parts'][-1]
+
+        if 'Interpretations' in node['label']['parts']:
+            m = m + '.'
+        else:
+            m = '(%s)' % m
+        return m
+
