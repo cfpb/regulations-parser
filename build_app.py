47e4818811e6474cd4b2dce9d6c8a63222e5b19f
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-06-18 17:29:12 +0000

    Removed the build app script

--- build_app.py
+++
@@ -1,6 +0,0 @@
-from parser.tree import xml_appendices
-
-reg_xml_file = '/vagrant/data/regulations/regulation/1005.xml'
-reg_xml = open(reg_xml_file, 'r').read()
-
-tree = xml_appendices.build_tree(reg_xml)
