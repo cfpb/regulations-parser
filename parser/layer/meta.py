5ea2881b3e88cf7ba407838f311b81eeaa8b1550
CM Lubinski <cm.lubinski@gmail.com>
2013-06-19 17:26:19 +0200

    Move cfr titles into settings

--- parser/layer/meta.py
+++ parser/layer/meta.py
@@ -3,60 +3,6 @@ import settings
 
 class Meta(Layer):
 
-    #   All current, US CFR titles
-    cfr_titles = [ None,
-        "General Provisions",
-        "Grants and Agreements",
-        "The President",
-        "Accounts",
-        "Administrative Personnel",
-        "Domestic Security",
-        "Agriculture",
-        "Aliens and Nationality",
-        "Animals and Animal Products",
-        "Energy",
-        "Federal Elections",
-        "Banks and Banking",
-        "Business Credit and Assistance",
-        "Aeronautics and Space",
-        "Commerce and Foreign Trade",
-        "Commercial Practices",
-        "Commodity and Securities Exchanges",
-        "Conservation of Power and Water Resources",
-        "Customs Duties",
-        "Employees' Benefits",
-        "Food and Drugs",
-        "Foreign Relations",
-        "Highways",
-        "Housing and Urban Development",
-        "Indians",
-        "Internal Revenue",
-        "Alcohol, Tobacco Products and Firearms",
-        "Judicial Administration",
-        "Labor",
-        "Mineral Resources",
-        "Money and Finance: Treasury",
-        "National Defense",
-        "Navigation and Navigable Waters",
-        "Education",
-        "Panama Canal [Reserved]",
-        "Parks, Forests, and Public Property",
-        "Patents, Trademarks, and Copyrights",
-        "Pensions, Bonuses, and Veterans' Relief",
-        "Postal Service",
-        "Protection of Environment",
-        "Public Contracts and Property Management",
-        "Public Health",
-        "Public Lands: Interior",
-        "Emergency Management and Assistance",
-        "Public Welfare",
-        "Shipping",
-        "Telecommunication",
-        "Federal Acquisition Regulations System",
-        "Transportation",
-        "Wildlife and Fisheries",
-    ]
-
     def __init__(self, tree, cfr_title, notices):
         Layer.__init__(self, tree)
         self.cfr_title = cfr_title
@@ -69,7 +15,7 @@ class Meta(Layer):
         if len(node['label']['parts']) == 1:
             layer = {
                 'cfr_title_number': self.cfr_title,
-                'cfr_title_text': Meta.cfr_titles[self.cfr_title]
+                'cfr_title_text': settings.CFR_TITLES[self.cfr_title]
             }
             if self.notices:
                 for dates in [n['dates']['effective'] for n in self.notices 
