58447835f2ff66b4423706a4ae5d2dbed0e7bddd
CM Lubinski <cm.lubinski@gmail.com>
2013-06-19 17:32:42 +0200

    Make the flow a bit clearer

--- parser/layer/meta.py
+++ parser/layer/meta.py
@@ -12,13 +12,14 @@ class Meta(Layer):
         """If this is the root element, add some 'meta' information about
         this regulation, including its cfr title, effective date, and any
         configured info"""
-        if len(node['label']['parts']) == 1:
-            layer = {
-                'cfr_title_number': self.cfr_title,
-                'cfr_title_text': settings.CFR_TITLES[self.cfr_title]
-            }
-            if self.notices:
-                for dates in [n['dates']['effective'] for n in self.notices 
-                        if 'dates' in n and 'effective' in n['dates']]:
-                    layer['effective_date'] = dates[-1]
-            return [dict(layer.items() + settings.META.items())]
+        if len(node['label']['parts']) != 1:
+            return
+
+        layer = {
+            'cfr_title_number': self.cfr_title,
+            'cfr_title_text': settings.CFR_TITLES[self.cfr_title]
+        }
+        for notice in self.notices:
+            if 'dates' in notice and 'effective' in notice['dates']:
+                layer['effective_date'] = notice['dates']['effective'][-1]
+        return [dict(layer.items() + settings.META.items())]
