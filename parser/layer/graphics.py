964b18b50b172d1ed4588adcfd25e4b0b5af7d23
CM Lubinski <cm.lubinski@gmail.com>
2013-06-21 00:39:22 +0200

    Graphics layer now used markdown syntax. Note! Alt text has the potential to break paragraph parsing now. This is a known bug

--- parser/layer/graphics.py
+++ parser/layer/graphics.py
@@ -3,7 +3,7 @@ from parser.layer.layer import Layer
 import re
 
 class Graphics(Layer):
-    gid = re.compile(ur'<GID>(.*?)</GID>', re.IGNORECASE)
+    gid = re.compile(ur'!\[([\w\s]+?)\]\(([a-zA-Z0-9.]+?)\)')
 
     def process(self, node):
         """If this node has a marker for an image in it, note where to get
@@ -18,7 +18,8 @@ class Graphics(Layer):
             layer_el.append({
                 'text': match.group(0),
                 'url': ('https://s3.amazonaws.com/images.federalregister.gov/'
-                    + match.group(1) + '/original.gif'),
+                    + match.group(2) + '/original.gif'),
+                'alt': match.group(1),
                 'locations': list(range(len(matches_by_text[text])))
             })
 
--- tests/layer_graphics_tests.py
+++ tests/layer_graphics_tests.py
@@ -5,19 +5,26 @@ from unittest import TestCase
 class LayerGraphicsTest(TestCase):
 
     def test_process(self):
-        node = struct.node("Testing <GID>ABCD</GID> then some more XXX " +
-            "some more <GID>XXX</GID> followed by <GID>ABCD</GID> and XXX")
+        node = struct.node("Testing ![ex](ABCD) then some more XXX " +
+            "some more ![222](XXX) followed by ![ex](ABCD) and XXX")
         g = Graphics(None)
         result = g.process(node)
         self.assertEqual(2, len(result))
         found = [False, False]
         for res in result:
-            if (res['text'] == '<GID>ABCD</GID>'
+            if (res['text'] == '![ex](ABCD)'
                 and 'ABCD' in res['url']
+                and res['alt'] == 'ex'
                 and res['locations'] == [0, 1]):
                 found[0] = True
-            elif (res['text'] == '<GID>XXX</GID>'
+            elif (res['text'] == '![222](XXX)'
                 and 'XXX' in res['url']
+                and res['alt'] == '222'
                 and res['locations'] == [0]):
                 found[1] = True
         self.assertEqual([True, True], found)
+
+    def test_process_format(self):
+        node = struct.node("![A88 Something](ER22MY13.257)")
+        g = Graphics(None)
+        self.assertEqual(1, len(g.process(node)))
