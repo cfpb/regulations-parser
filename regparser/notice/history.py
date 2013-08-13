8507e224f970648cb05bb7ca940618825b65af66
CM Lubinski <cm.lubinski@gmail.com>
2013-08-13 22:32:43 +0200

    Function in notice.history to deal with finding applicable notices

--- regparser/notice/history.py
+++ regparser/notice/history.py
@@ -57,3 +57,23 @@ def altered_frs(sent):
     if tokens:
         changed_to = tokens[-1]
     return frs, changed_to
+
+
+def applicable(notices, doc_number):
+    """Given a list of notices and a specific notice number, determine which
+    notices in the list are relevant to that doc number."""
+
+    final_notice = [n for n in notices if n['document_number']]
+    if not final_notice:    #   We need the notice for that doc_number
+        return []
+    final_notice = final_notice[0]
+
+    include = [final_notice]
+    #   @todo: this doesn't take into account proposals as final_notice, nor
+    #   does it include proposals in the list
+    for notice in [n for n in notices if 'effective_on' in n]:
+        if notice['effective_on'] < final_notice['effective_on'] or (
+            notice['effective_on'] == final_notice['effective_on']
+            and notice['publication_date'] < final_notice['publication_date']):
+            include.append(notice)
+    return include
---
+++ tests/notice_history_tests.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+
+from regparser.notice.history import *
+
+class NoticeHistoryTests(TestCase):
+
+    def test_applicable(self):
+        head = {    'document_number': 'head', 
+                    'effective_on': '2012-05-05',
+                    'publication_date': '2011-09-09' }
+        history = { 'document_number': 'history',
+                    'effective_on': '2012-01-01',
+                    'publication_date': '2011-04-04' }
+        prefinal = {'document_number': 'pre_final',
+                    'effective_on': '2012-05-05',
+                    'publication_date': '2011-08-08' }
+        future = {  'document_number': 'future',
+                    'effective_on': '2012-05-05',
+                    'publication_date': '2011-10-10' }
+        notices = applicable([head, history, prefinal, future], 'head')
+        self.assertEqual([head, history, prefinal], notices)
+
+    def test_applicable_proposal(self):
+        """For now, we ignore proposals"""
+        head = {    'document_number': 'head', 
+                    'effective_on': '2012-05-05',
+                    'publication_date': '2011-09-09' }
+        proposal = {'document_number': 'proposal',
+                    'publication_date': '2011-08-18' }
+        notices = applicable([head, proposal], 'head')
+        self.assertEqual([head], notices)
