32ac7165f0834b215402fb46691b7e6401fe3f22
CM Lubinski <cm.lubinski@gmail.com>
2013-09-26 15:49:03 +0200

    Sometimes the supplement has a different title

--- regparser/tree/supplement.py
+++ regparser/tree/supplement.py
@@ -4,4 +4,6 @@ from regparser.search import find_start
 
 def find_supplement_start(text, supplement='I'):
     """Find the start of the supplement (e.g. Supplement I)"""
-    return find_start(text, 'Supplement', supplement)
+    supplement = find_start(text, 'Supplement', supplement)
+    commentary = find_start(text, 'Official Commentary on', '')
+    return supplement or commentary
