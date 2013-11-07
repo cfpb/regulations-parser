97e67a5d1c6809680e2963ec17926d7e40f8b82c
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-11-07 21:39:14 +0000

    First tests

--- tests/tree_xml_parser_reg_text_tests.py
+++ tests/tree_xml_parser_reg_text_tests.py
@@ -25,3 +25,64 @@ class RegTextTest(TestCase):
         self.assertEqual('(a) something something', child.text.strip())
         self.assertEqual([], child.children)
         self.assertEqual(['8675', '309', 'a'], child.label)
+
+    def test_get_title(self):
+        xml = u"""
+            <PART>
+                <HD>regulation title</HD>
+            </PART>"""
+        title = get_title(etree.fromstring(xml))
+        self.assertEqual(u'regulation title', title)
+
+    def test_get_reg_part(self):
+        xml = u"""
+            <PART>
+                <EAR> Pt. 204 </EAR>
+            </PART>
+        """
+        part = get_reg_part(etree.fromstring(xml))
+        self.assertEqual(part, '204')
+
+    def test_get_reg_part_fr_notice_style(self):
+        xml = u"""
+            <REGTEXT PART="204">
+            <SECTION>
+            </SECTION>
+            </REGTEXT>
+        """
+        part = get_reg_part(etree.fromstring(xml))
+        self.assertEqual(part, '204')
+
+
+    def test_get_subpart_title(self):
+        xml = u"""
+            <SUBPART>
+                <HD>Subpart A—First subpart</HD>
+            </SUBPART>"""
+        subpart_title = get_subpart_title(etree.fromstring(xml))
+        self.assertEqual(subpart_title, u'Subpart A—First subpart')
+
+    def test_build_subpart(self):
+        xml = u"""
+            <SUBPART>
+                <HD>Subpart A—First subpart</HD>
+            <SECTION>
+                <SECTNO>§ 8675.309</SECTNO>
+                <SUBJECT>Definitions.</SUBJECT>
+                <P>Some content about this section.</P>
+                <P>(a) something something</P>
+            </SECTION>
+            <SECTION>
+                <SECTNO>§ 8675.310 </SECTNO>
+                <SUBJECT>Definitions.</SUBJECT>
+                <P>Some content about this section.</P>
+                <P>(a) something something</P>
+            </SECTION>
+            </SUBPART>
+        """
+        subpart = build_subpart('8675', etree.fromstring(xml))
+        self.assertEqual(subpart.node_type, 'subpart')
+        self.assertEqual(len(subpart.children), 2)
+        self.assertEqual(subpart.label, ['8675', 'Subpart', 'A'])
+        child_labels = [c.label for c in subpart.children]
+        self.assertEqual([['8675', '309'], ['8675', '310']], child_labels)
