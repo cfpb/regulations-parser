fe56245320a3efec93cdd6b93e98e33544986768
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-12-24 15:13:05 +0000

    Just call self.tokens iterator to make TokenList iterable

--- regparser/grammar/tokens.py
+++ regparser/grammar/tokens.py
@@ -91,7 +91,6 @@ class TokenList:
 
     def __init__(self, tokens):
         self.tokens = tokens
-        self.current = 0
 
     def __repr__(self):
         return "TokenList([ %s ])" % ', '.join(map(repr, self.tokens))
@@ -100,12 +99,4 @@ class TokenList:
         return repr(self) == repr(other)
 
     def __iter__(self):
-        return self
-
-    def next(self):
-        """ Make TokenList iteratable. """
-        if self.current > len(self.tokens) - 1:
-            raise StopIteration
-        else:
-            self.current += 1
-            return self.tokens[self.current - 1]
+        return iter(self.tokens)
