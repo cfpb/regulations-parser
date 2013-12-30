d17eb4e55c5bd52ad089e58d21dd73a805e4ad3c
Theresa Summa <theresa.summa@cfpb.gov>
2013-12-30 20:16:14 -0200

    pass term and position to add_match explicitly

--- regparser/layer/terms.py
+++ regparser/layer/terms.py
@@ -111,10 +111,7 @@ class Terms(Layer):
         included_defs = []
         excluded_defs = []
 
-        def add_match(n, match):
-            term = match.term.tokens[0].lower()
-            pos = match.term.pos
-
+        def add_match(n, term, pos):
             add_to = included_defs
 
             if term == 'act' and list(uscode.scanString(n.text)):
@@ -125,17 +122,20 @@ class Terms(Layer):
 
         def per_node(n):
             for match, _, _ in term_parser.scanString(n.text):
-                add_match(n, match)
+                add_match(n,
+                          match.term.tokens[0].lower(),
+                          match.term.pos)
 
             if hasattr(n, 'tagged_text'):
                 for match, _, _ in xml_term_parser.scanString(n.tagged_text):
-                    """Returned position tuple has positions relating
-                    to n.tagged_text. Replace with positions that apply
-                    to n.text."""
+                    """Position in match reflects XML tags, so its
+                    dropped in preference of new values based on
+                    n.text."""
                     pos_start = n.text.find(match.term.tokens[0])
                     match_len = len(match.term.tokens[0])
-                    match.term.pos = (pos_start, pos_start + match_len)
-                    add_match(n, match)
+                    add_match(n,
+                              match.term.tokens[0].lower(),
+                              (pos_start, pos_start + match_len))
 
         struct.walk(node, per_node)
         return included_defs, excluded_defs
