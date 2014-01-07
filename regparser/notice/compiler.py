becc5afb53acb3abfdf420a2a46c42c37558c86d
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2014-01-07 18:03:36 +0000

    Enough code to compile one version after the reissuance of E

--- regparser/notice/compiler.py
+++ regparser/notice/compiler.py
@@ -1,5 +1,6 @@
 import copy
-from regparser.tree.struct import Node
+from regparser.tree.struct import Node, find
+from regparser.utils import roman_nums
 
 """ Notices indicate how a regulation has changed since the last version. This
 module contains code to compile a regulation from a notice's changes. """
@@ -8,17 +9,45 @@ class RegulationTree(object):
     def __init__(self, previous_tree):
         self.tree = copy.deepcopy(previous_tree)
 
-    def replace_node_and_subtree(self, node):
-        """ Replace the whole node with the one in change. """
-        #find parent of node
+    def get_parent_label(self, node):
         parent_label = node.label[:-1]
+        return '-'.join(parent_label)
 
-        #remove node from children list
+    def make_label_sortable(self, label, roman=False):
+        """ Make labels sortable, but converting them as appropriate. 
+        Also, appendices have labels that look like 30(a), we make those 
+        appropriately sortable. """
 
-        #add node to list
+        if label.isdigit():
+            return (int(label),)
+        if label.isalpha():
+            if roman:
+                romans = list(itertools.islice(roman_nums(), 0, 50))
+                return 1 + romans.index(label)
+            else:
+                return (label,)
+        else:
+            m = re.match(r"([0-9]+)([\(])([a-z]+)([\)])", label, re.I)
+            return (int(m.groups()[0]), m.groups()[2])
 
-        #sort children
+    def add_child(self, children, node):
+        children.append(node)
 
+        for c in children:
+            c.sortable = self.make_label_sortable(
+                c.label[-1], roman=(len(c.label) == 5))
+
+        children.sort(key=lambda x: x.sortable)
+        return children
+
+    def replace_node_and_subtree(self, node):
+        """ Replace an existing node in the tree with node.  """
+        #find parent of node
+        parent_label = self.get_parent_label(node)
+        parent = find(self.tree, parent_label) 
+
+        other_children = [c for c in parent.children if c.label != node.label]
+        parent.children = self.add_child(other_children, node)
 
 def dict_to_node(node_dict):
     """ Convert a dictionary representation of a node into a Node object if 
@@ -48,3 +77,6 @@ def compile_regulation(previous_tree, notice_changes):
             if change['action'] == 'PUT' and replace_subtree:
                 node = dict_to_node(change['node'])
                 reg.replace_node_and_subtree(node)
+            else:
+                print label
+    return reg
