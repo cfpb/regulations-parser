cc0d3c29ec865f669ed6f1e701c089f13b217ad7
CM Lubinski <cm.lubinski@gmail.com>
2014-01-14 18:58:01 +0100

    Add encoding for amendments

---
+++ regparser/notice/encoder.py
@@ -0,0 +1,11 @@
+from json import JSONEncoder
+
+from regparser.notice.diff import Amendment
+
+
+class AmendmentEncoder(JSONEncoder):
+    """Custom JSON encoder to handle Amendment objects"""
+    def default(self, obj):
+        if isinstance(obj, Amendment):
+            return repr(obj)
+        return super(NodeEncoder, self).default(self, obj)
