4a5e2f2ccc9c910790d252e900e3599d5dd2682b
CM Lubinski <cm.lubinski@gmail.com>
2014-02-03 23:53:55 +0100

    Additional parsing for interpretations that do not have headers

--- regparser/notice/build_interp.py
+++ regparser/notice/build_interp.py
@@ -2,8 +2,13 @@ from copy import deepcopy
 
 from regparser.notice.diff import DesignateAmendment
 from regparser.notice.util import spaces_then_remove
-from regparser.tree.struct import Node
-from regparser.tree.xml_parser import interpretations
+from regparser.tree.struct import Node, treeify
+from regparser.tree.xml_parser import interpretations, tree_utils
+
+
+def _is_interp_amend(al):
+    return (not isinstance(al, DesignateAmendment)
+            and Node.INTERP_MARK in al.label)
 
 
 def parse_interp_changes(amended_labels, cfr_part, parent_xml):
@@ -11,26 +16,31 @@ def parse_interp_changes(amended_labels, cfr_part, parent_xml):
     interp tree with the assumption that interpretation headers (e.g.
     "22(b)") are present; if that doesn't work, try matching paragraphs by
     looking at the amended labels"""
-    if any(not isinstance(al, DesignateAmendment)
-           and Node.INTERP_MARK in al.label for al in amended_labels):
-        interp = process_with_headers(cfr_part, parent_xml)
-        return interp
+    if any(_is_interp_amend(al) for al in amended_labels):
+        return (
+            process_with_headers(cfr_part, parent_xml)
+            or process_without_headers(cfr_part, parent_xml, amended_labels))
 
 
-def process_with_headers(cfr_part, parent_xml):
-    """Figure out which parts of the parent_xml are relevant to
-    interpretations. Pass those on to interpretations.parse_from_xml and
-    return the results"""
-    # First, we try to standardize the xml. We will assume a format of
-    # Supplement I header followed by HDs, STARS, and Ps.
-    parent_xml = spaces_then_remove(deepcopy(parent_xml), 'PRTPAGE')
-    for extract in parent_xml.xpath(".//EXTRACT"):
+def standardize_xml(xml):
+    """We will assume a format of Supplement I header followed by HDs,
+    STARS, and Ps, so move anything in an EXTRACT up a level"""
+    xml = spaces_then_remove(deepcopy(xml), 'PRTPAGE')
+    for extract in xml.xpath(".//EXTRACT"):
         ex_parent = extract.getparent()
         idx = ex_parent.index(extract)
         for child in extract:
             ex_parent.insert(idx, child)
             idx += 1
         ex_parent.remove(extract)
+    return xml
+
+
+def process_with_headers(cfr_part, parent_xml):
+    """Figure out which parts of the parent_xml are relevant to
+    interpretations. Pass those on to interpretations.parse_from_xml and
+    return the results"""
+    parent_xml = standardize_xml(parent_xml)
 
     # Skip over everything until 'Supplement I' in a header
     seen_header = False
@@ -46,4 +56,50 @@ def process_with_headers(cfr_part, parent_xml):
                 seen_header = True
 
     root = Node(label=[cfr_part, Node.INTERP_MARK], node_type=Node.INTERP)
-    return interpretations.parse_from_xml(root, xml_nodes)
+    root = interpretations.parse_from_xml(root, xml_nodes)
+    if not root.children:
+        return None
+    else:
+        return root
+
+
+def process_without_headers(cfr_part, parent_xml, amended_labels):
+    """Sometimes, we only get a list of paragraphs that have changes, but no
+    header indicating with which sections they are associated. Accommodate
+    by trying to match up amended_labels with paragraphs"""
+    parent_xml = standardize_xml(parent_xml)
+
+    relevant_labels = [al.label for al in 
+                       filter(_is_interp_amend, amended_labels)]
+    label_indices = []
+    for idx, child in enumerate(parent_xml):
+        text = tree_utils.get_node_text(child)
+        if len(relevant_labels) > len(label_indices):
+            marker = relevant_labels[len(label_indices)][-1] + '.'
+            if text.startswith(marker):
+                label_indices.append(idx)
+
+    labelXindex = zip(relevant_labels, label_indices)
+    nodes = []
+    #   Reverse it so we can delete from the bottom
+    for label, idx in reversed(labelXindex):
+        stack = tree_utils.NodeStack()
+        prefix = label[:label.index(Node.INTERP_MARK) + 1]
+        section = Node(node_type=Node.INTERP, label=prefix)
+        stack.add(2, section)
+        interpretations.process_inner_children(stack, parent_xml[idx - 1])
+        while stack.size() > 1:
+            stack.unwind()
+
+        nodes.append(stack.m_stack[0][0][1])
+
+        # delete the tail
+        while len(parent_xml.getchildren()) > idx:
+            parent_xml.remove(parent_xml[idx])
+    if nodes:
+        nodes.append(Node(node_type=Node.INTERP,
+                          label=[cfr_part, Node.INTERP_MARK]))
+        #   Reverse it again into normal flow
+        return treeify(list(reversed(nodes)))[0]
+    else:
+        return None
--- tests/notice_build_interp_tests.py
+++ tests/notice_build_interp_tests.py
@@ -5,6 +5,7 @@ from lxml import etree
 from mock import patch
 
 from regparser.notice import build_interp
+from regparser.notice.diff import Amendment
 
 
 class NoticeBuildInterpTest(TestCase):
@@ -61,3 +62,37 @@ class NoticeBuildInterpTest(TestCase):
             self.assertEqual(root.label, ['111', 'Interp'])
             self.assertEqual(['HD', 'T1', 'P'], [n.tag for n in nodes])
 
+    def test_process_without_headers(self):
+        xml = """
+            <REGTEXT>
+                <AMDPAR>Adding comment 33(c)-5, 34(b)-5, and 34(b)-6</AMDPAR>
+                <P>5. five five five</P>
+                <P>i. eye eye eye</P>
+                <P>5. five five five2</P>
+                <P>6. six six six</P>
+            </REGTEXT>"""
+        amended_labels = [Amendment('POST', '111-Interpretations-33-(c)-5'),
+                          Amendment('POST', '111-Interpretations-34-(b)-5'),
+                          Amendment('POST', '111-Interpretations-34-(b)-6')]
+        interp = build_interp.process_without_headers(
+            '111', etree.fromstring(xml), amended_labels)
+        self.assertEqual(2, len(interp.children))
+        c, b = interp.children
+        self.assertEqual(c.label, ['111', '33', 'c', 'Interp'])
+        self.assertEqual(1, len(c.children))
+        c5 = c.children[0]
+        self.assertEqual('5. five five five', c5.text.strip())
+        self.assertEqual(c5.label, ['111', '33', 'c', 'Interp', '5'])
+        self.assertEqual(1, len(c5.children))
+        c5i = c5.children[0]
+        self.assertEqual('i. eye eye eye', c5i.text.strip())
+        self.assertEqual(c5i.label, ['111', '33', 'c', 'Interp', '5', 'i'])
+        self.assertEqual([], c5i.children)
+
+        b5, b6 = b.children
+        self.assertEqual('5. five five five2', b5.text.strip())
+        self.assertEqual(b5.label, ['111', '34', 'b', 'Interp', '5'])
+        self.assertEqual([], b5.children)
+        self.assertEqual('6. six six six', b6.text.strip())
+        self.assertEqual(b6.label, ['111', '34', 'b', 'Interp', '6'])
+        self.assertEqual([], b6.children)
