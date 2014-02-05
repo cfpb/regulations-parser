5efc2d7b09ca656b41d402e3b54cc552a41d6dee
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2014-02-05 18:12:34 +0000

    Use plaintext paragraph marker

--- regparser/grammar/amdpar.py
+++ regparser/grammar/amdpar.py
@@ -259,7 +259,7 @@ multiple_paragraphs = (
     (atomic.paragraphs_marker | atomic.paragraph_marker)
     + make_multiple(unified.any_depth_p)
     ).setParseAction(make_par_list(lambda m: [m.part, None, m.section,
-        m.p1, m.p2, m.p3, m.p4, m.p5]))
+        m.p1, m.p2, m.p3, m.p4, m.plaintext_p5]))
 
 
 #   grammar which captures all of these possibilities
