cb40652640c5c5f7b4bd774ee50fa9b73fd4af72
CM Lubinski <cm.lubinski@gmail.com>
2014-03-18 22:02:19 +0100

    Add spaces to table headers and cells

--- tests/layer_formatting_tests.py
+++ tests/layer_formatting_tests.py
@@ -19,7 +19,7 @@ class LayerFormattingTests(TestCase):
                 <CHED H="2">2-2</CHED>
                 <CHED H="3">3-4</CHED>
                 <CHED H="3">3-5</CHED>
-                <CHED H="3">3-6</CHED>
+                <CHED H="3">3-6<LI>Other Content</LI></CHED>
             </BOXHD>""")
         root = formatting.build_header(xml.xpath('./CHED'))
 
@@ -53,7 +53,7 @@ class LayerFormattingTests(TestCase):
         n34, n35, n36 = n22.children
         self.assertEqual('3-4', n34.text)
         self.assertEqual('3-5', n35.text)
-        self.assertEqual('3-6', n36.text)
+        self.assertEqual('3-6 Other Content', n36.text)
         for n in n22.children:
             self.assertEqual(1, n.colspan)
             self.assertEqual(1, n.rowspan)
@@ -68,13 +68,17 @@ class LayerFormattingTests(TestCase):
                     <CHED H="3">3-1</CHED>
                     <CHED H="2">2-2</CHED>
                     <CHED H="3">3-2</CHED>
-                    <CHED H="3">3-3</CHED>
+                    <CHED H="3">3-3<LI>Content</LI>Here</CHED>
                 </BOXHD>
                 <ROW><ENT>11</ENT><ENT>12</ENT><ENT>13</ENT><ENT>14</ENT></ROW>
                 <ROW><ENT>21</ENT><ENT>22</ENT><ENT>23</ENT></ROW>
-                <ROW><ENT /><ENT>32</ENT><ENT>33</ENT><ENT>34</ENT></ROW>
+                <ROW>
+                    <ENT /><ENT>32</ENT><ENT>33<E>More</E></ENT><ENT>34</ENT>
+                </ROW>
             </GPOTABLE>""")
         markdown = formatting.table_xml_to_plaintext(xml)
+        self.assertTrue("3-3 Content Here" in markdown)
+        self.assertTrue("33 More" in markdown)
         node = Node(markdown, source_xml=xml)
         result = formatting.Formatting(None).process(node)
         self.assertEqual(1, len(result))
@@ -89,12 +93,13 @@ class LayerFormattingTests(TestCase):
             data['header'],
             [[mkhd('1-1', 1, 3), mkhd('1-2', 3, 1)],
              [mkhd('2-1', 1, 1), mkhd('2-2', 2, 1)],
-             [mkhd('3-1', 1, 1), mkhd('3-2', 1, 1), mkhd('3-3', 1, 1)]])
+             [mkhd('3-1', 1, 1), mkhd('3-2', 1, 1),
+              mkhd('3-3 Content Here', 1, 1)]])
         self.assertEqual(
             data['rows'],
             [['11', '12', '13', '14'],
              ['21', '22', '23'],
-             ['', '32', '33', '34']])
+             ['', '32', '33 More', '34']])
 
     def test_process_fenced(self):
         node = Node("Content content\n```abc def\nLine 1\nLine 2\n```")
