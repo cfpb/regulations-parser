b36ef1532fe0891178cffc78fad8a4fd74a7a283
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2014-03-24 18:59:31 +0000

    PEP8

--- tests/layer_terms_tests.py
+++ tests/layer_terms_tests.py
@@ -9,7 +9,7 @@ class LayerTermTest(TestCase):
 
     def setUp(self):
         self.original_ignores = settings.IGNORE_DEFINITIONS_IN
-        settings.IGNORE_DEFINITIONS_IN = {'ALL':{}}
+        settings.IGNORE_DEFINITIONS_IN = {'ALL': {}}
 
     def tearDown(self):
         settings.IGNORE_DEFINITIONS_IN = self.original_ignores
@@ -342,7 +342,6 @@ class LayerTermTest(TestCase):
             [('1000', '3'), ('1000', '3', Node.INTERP_MARK)],
             t.determine_scope(stack))
 
-
     def test_pre_process(self):
         noname_subpart = Node(
             '',
@@ -471,7 +470,7 @@ class LayerTermTest(TestCase):
         excluded = t.per_regulation_ignores(
             exclusions, ['12', '2'], 'There is a consumer price index')
         self.assertEqual([(0, 4), (11, 31)], excluded)
-        
+
     def test_excluded_offsets_blacklist_word_boundaries(self):
         t = Terms(None)
         t.scoped_terms['_'] = [Ref('act', '28-6-d', 'Def def def')]
