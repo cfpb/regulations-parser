ccfe26b0ac58b806d308f20724dfad10e8dd7ed2
Will Barton <william.barton@cfpb.gov>
2015-07-15 09:42:48 -0400

    Fix multi-alpha p1 matching and tests

--- tests/grammar_atomic_tests.py
+++ tests/grammar_atomic_tests.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+from pyparsing import ParseException
 from regparser.grammar.atomic import *
 
 
@@ -7,3 +8,21 @@ class GrammarAtomicTests(TestCase):
     def test_em_digit_p(self):
         result = em_digit_p.parseString('(<E T="03">2</E>)')
         self.assertEqual('2', result.p5)
+
+    def test_double_alpha(self):
+        # Match (aa), (bb), etc.
+        result = lower_p.parseString('(a)')
+        self.assertEqual('a', result.p1)
+
+        result = lower_p.parseString('(aa)')
+        self.assertEqual('aa', result.p1)
+
+        result = lower_p.parseString('(i)')
+        self.assertEqual('i', result.p1)
+
+        # Except for roman numerals
+        with self.assertRaises(ParseException):
+            result = lower_p.parseString('(ii)')
+        with self.assertRaises(ParseException):
+            result = lower_p.parseString('(iv)')
+
