019aa0d78fbf54dda405cf8df3aab92dfdaba188
Will Barton <william.barton@cfpb.gov>
2015-07-15 10:50:28 -0400

    Refactor test to eliminate assertRaises() error with Python 2.6

--- tests/grammar_atomic_tests.py
+++ tests/grammar_atomic_tests.py
@@ -10,19 +10,19 @@ class GrammarAtomicTests(TestCase):
         self.assertEqual('2', result.p5)
 
     def test_double_alpha(self):
-        # Match (aa), (bb), etc.
-        result = lower_p.parseString('(a)')
-        self.assertEqual('a', result.p1)
+        for text, p1 in [('(a)', 'a'),
+                     ('(aa)', 'aa'),
+                     ('(i)','i')]:
+            result = lower_p.parseString(text)
+            self.assertEqual(p1, result.p1)
 
-        result = lower_p.parseString('(aa)')
-        self.assertEqual('aa', result.p1)
-
-        result = lower_p.parseString('(i)')
-        self.assertEqual('i', result.p1)
-
-        # Except for roman numerals
-        with self.assertRaises(ParseException):
-            result = lower_p.parseString('(ii)')
-        with self.assertRaises(ParseException):
-            result = lower_p.parseString('(iv)')
+        for text in ['(ii)', '(iv)', '(vi)']:
+            try:
+                result = lower_p.parseString(text)
+            except ParseException:
+                pass
+            except e:
+                self.fail("Unexpected error:", e)
+            else:
+                self.fail("Didn't raise ParseException")
 
