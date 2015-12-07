b2b6d446878ab2805ce32aac7993bb4f4d8a5c2d
Will Barton <william.barton@cfpb.gov>
2015-12-07 08:32:24 -0500

    Fix some tests

--- regparser/notice/build.py
+++ regparser/notice/build.py
@@ -306,7 +306,7 @@ def process_amendments(notice, notice_xml):
         else:
             amdpars_by_parent.append(AmdparByParent(parent, par))
 
-    default_cfr_part = notice['cfr_part']
+    default_cfr_part = notice['cfr_parts'][0]
     for aXp in amdpars_by_parent:
         amended_labels = []
         designate_labels, other_labels = [], []
@@ -331,12 +331,11 @@ def process_amendments(notice, notice_xml):
 
             create_xmlless_changes(other_labels, notice_changes)
 
-            # for cfr_part, rel_labels in labels_by_part.iteritems():
-            labels_for_part = {part: labels
-                               for part, labels in labels_by_part.iteritems()
-                               if part == default_cfr_part}
-
-            for cfr_part, rel_labels in labels_for_part.iteritems():
+            for cfr_part, rel_labels in labels_by_part.iteritems():
+            # labels_for_part = {part: labels
+            #                    for part, labels in labels_by_part.iteritems()
+            #                    if part == default_cfr_part}
+            # for cfr_part, rel_labels in labels_for_part.iteritems():
                 section_xml = find_section(par)
                 if section_xml is not None:
                     subparts = aXp.parent.xpath('.//SUBPART/HD')
